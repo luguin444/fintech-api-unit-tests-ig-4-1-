fe1d0c46c0b5c922b7d7e60f0707045b0530188b
Luis Gustavo <luguin444@gmail.com>
2022-07-21 22:14:15 -0300

    fix: erase console log

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -31,7 +31,6 @@ export class AuthenticateUserUseCase {
     }
 
     const passwordMatch = await compare(password, user.password);
-    // const passwordMatch = false;
 
     if (!passwordMatch) {
       throw new IncorrectEmailOrPasswordError();
